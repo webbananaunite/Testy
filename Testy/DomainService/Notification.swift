1f7df653788d79077f74fa1651ab6ba188954c6d
webbananaunite <mizu@webbanana.org>
2024-01-11 18:17:58 +0900

    Make Notify to user for have joined network.

---
+++ Testy/DomainService/Notification.swift
@@ -0,0 +1,9 @@
+//
+//  Notification.swift
+//  Testy
+//
+//  Created by よういち on 2024/01/11.
+//  Copyright © 2024 WEB BANANA UNITE Tokyo-Yokohama LPC. All rights reserved.
+//
+
+import Foundation
